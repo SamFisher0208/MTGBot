54ca6e0ea69dac79f82ae3eafde71fb9c340479e
FireAndIrons <sammee1@gmail.com>
2022-04-05 13:38:26 -0500

    second try

---
+++ main.py
@@ -0,0 +1,66 @@
+import discord  #discord library
+import os
+from dotenv import load_dotenv
+import sqlite3
+import postCards
+#from keepAlive import keepAlive
+
+
+#connection = sqlite3.connect("magicRepeats.db")
+#cursr = connection.cursor()
+#print("Connected to the database")
+#connection.close()
+
+
+client = discord.Client()
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+@client.event
+async def on_ready():
+    #this just changes the status of the bot
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='[Scryfall]'))
+    print('We have logged in as {0.user}'.format(client))
+      
+    
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("!randcard") or message.content.startswith("!vibes"):
+    await message.channel.send(postCards.getVibe())
+  
+  if message.content.startswith("!randcreature"):
+    await message.channel.send(postCards.getCreature())
+
+  if message.content.startswith("!randinstant"):
+    await message.channel.send(postCards.getInstant())
+
+  if message.content.startswith("!randsorcery"):
+    await message.channel.send(postCards.getSorcery())
+
+  if message.content.startswith("!castinstant"):
+    for member in message.mentions:
+      victim = member.mention
+    
+    cast = f"{victim} was casted upon!\n{postCards.getInstant()}"
+    await message.channel.send(cast)
+
+  if message.content.startswith("!castsorcery"):
+    for member in message.mentions:
+      victim = member.mention
+    
+    cast = f"{victim} was casted upon!\n{postCards.getSorcery()}"
+    await message.channel.send(cast)
+  
+  #if message.content.startswith("[["):
+
+    
+
+#keepAlive()
+
+client.run(TOKEN)
+
---
+++ postCards.py
@@ -0,0 +1,23 @@
+import requests #http library
+import json #json library
+
+def getCard(cardType):
+  card = requests.get(f"https://api.scryfall.com/cards/random{cardType}")
+  jsonData = json.loads(card.text)
+  imgURL = jsonData['image_uris']['png']
+  return imgURL
+
+def getVibe():
+  card = requests.get("https://api.scryfall.com/cards/random")
+  jsonData = json.loads(card.text)
+  imgURL = jsonData['image_uris']['png']
+  return imgURL
+
+def getCreature():
+  return getCard("?q=type%3Acreature")
+
+def getInstant():
+  return getCard("?q=type%3Ainstant")
+
+def getSorcery():
+  return getCard("?q=type%3Asorcery")
