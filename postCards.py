f65c9cba1167f0270a8c92e788983d4c334d0007
Sam Fisher <67036557+SamFisher0208@users.noreply.github.com>
2026-08-13 23:18:39 -0700

    silencing prints

--- postCards.py
+++ postCards.py
@@ -5,10 +5,10 @@ import json #json library
 def getCard(cardType):
   headers = {'user-agent': 'ItsJustMeBeech/MTGBot/1.0', 'Accept': '*/*'}
   card = requests.get(f"https://api.scryfall.com/cards/random{cardType}", headers=headers)
-  print('hello world')
-  print(card)
+  #print('hello world')
+  #print(card)
   jsonData = json.loads(card.text)
-  print(jsonData)
+  #print(jsonData)
   imgURL = jsonData['image_uris']['png']
   #a change
   return imgURL
